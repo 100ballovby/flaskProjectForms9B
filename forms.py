21f446e2a716780de8ae06979d6f117132e3b9ae
demidraksin <greatraksin@icloud.com>
2021-01-13 11:20:51 +0300

    Adding form configuration Class

--- forms.py
+++ forms.py
@@ -1,2 +1,13 @@
 from flask_wtf import FlaskForm
 # flask web text forms
+from wtforms import StringField, TextAreaField, SubmitField
+from wtforms.validators import DataRequired, Length
+
+
+class ContactForm(FlaskForm):
+    """Contact Form"""
+    name = StringField('Имя', [DataRequired()])
+    email = StringField('Email', [DataRequired()])
+    body = TextAreaField('Текст', [DataRequired(),
+                                   Length(min=5, message='Your message is too short')])
+    submit = SubmitField('Отправить')
