c8d28b8366114daa4404f34bd1699cae0c43f59f
demidraksin <greatraksin@icloud.com>
2021-01-13 12:14:22 +0300

    Adding some style⭐️

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
-from flask import Flask
+from flask import Flask, url_for, redirect, render_template
+from forms import ContactForm
 
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def hello_world():
-    return 'Hello World!'
+    form = ContactForm()
+    if form.validate_on_submit():
+        return redirect(url_for('success'))
+    return render_template('index.html', form=form)
 
 
 if __name__ == '__main__':
