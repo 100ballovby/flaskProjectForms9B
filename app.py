4c4e965721c52080e36973cea069e7839334c057
demidraksin <greatraksin@icloud.com>
2021-01-20 11:23:30 +0300

    Adding success and index pages✨

--- app.py
+++ app.py
@@ -6,12 +6,17 @@ app = Flask(__name__, instance_relative_config=False)
 app.config.from_object('config.Config')
 
 
-@app.route('/', methods=['GET', 'POST'])
-def hello_world():
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
     form = ContactForm()
     if form.validate_on_submit():
         return redirect(url_for('success'))
     return render_template('forms.html', form=form)
+
+
+@app.route('/')
+def main_page():
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
